b07ac2972a214b76f119919faaad0fff70c85f9d
Jarwesbub <jarwes90@gmail.com>
2025-02-28 14:33:37 +0200

    Added mobile and desktop views, removed debug text from the upper corner

--- lib/widgets/custom_appbar.dart
+++ lib/widgets/custom_appbar.dart
@@ -2,12 +2,19 @@ import 'package:flutter/material.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CustomAppBar({super.key, required this.title});
-
   final String title;
 
   @override
   Widget build(BuildContext context) {
     final bool showBackButton = Navigator.canPop(context);
+    if (MediaQuery.of(context).size.width < 701) {
+      // User mobile view.
+      return mobileView(context, showBackButton);
+    }
+    return desktopView(context);
+  }
+
+  Widget mobileView(BuildContext context, showBackButton) {
     return AppBar(
       automaticallyImplyLeading: false,
       title: Text(title),
@@ -16,8 +23,9 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
       foregroundColor: Theme.of(context).colorScheme.onTertiary,
       actions: [
         IconButton(
+          // User profile button.
           onPressed: () {
-            debugPrint('Pressed profile button');
+            debugPrint('Pressed profile button (mobile)');
           },
           icon: const Icon(Icons.toc_outlined, size: 40),
         ),
@@ -27,10 +35,41 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
           showBackButton
               ? IconButton(
                 // Back button.
-                onPressed: () => Navigator.pop(context),
-                icon: const Icon(Icons.arrow_left),
+                onPressed: () {
+                  if (Navigator.canPop(context)) {
+                    // Checks if the current view can be closed.
+                    Navigator.pop(context);
+                  }
+                },
+                icon: const Icon(Icons.arrow_back, size: 30),
               )
               : null,
+    );
+  }
+
+  Widget desktopView(BuildContext context) {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      title: Text(title, style: TextStyle(color: Colors.white, fontSize: 24)),
+      centerTitle: true,
+      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      foregroundColor: Theme.of(context).colorScheme.onTertiary,
+      actions: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: TextButton(
+            // User profile button.
+            onPressed: () {
+              debugPrint('Pressed profile button (desktop)');
+            },
+            child: Text(
+              'Profile',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ),
+        ),
+      ],
+      leading: null,
     );
   }
 
