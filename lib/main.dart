46bf82dcf024cbe486f942a61c4469d95cdda98c
Jarwesbub <jarwes90@gmail.com>
2025-03-19 11:05:31 +0200

    Updated asset file folders, added empty healthcare_service json file

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,7 @@ import 'package:bubble/bubble.dart';
 import 'package:dialogflow_flutter_plus/dialogflowFlutter.dart';
 import 'package:dialogflow_flutter_plus/googleAuth.dart';
 import 'package:dialogflow_flutter_plus/language.dart';
-import 'package:dialogflow_flutter_plus/message.dart';
 import 'package:intl/intl.dart';
-
 
 void main() {
   runApp(MyApp());
@@ -15,12 +13,18 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      scrollBehavior: MaterialScrollBehavior().copyWith( dragDevices: {PointerDeviceKind.mouse, PointerDeviceKind.touch, PointerDeviceKind.stylus, PointerDeviceKind.unknown}),
+      scrollBehavior: MaterialScrollBehavior().copyWith(
+        dragDevices: {
+          PointerDeviceKind.mouse,
+          PointerDeviceKind.touch,
+          PointerDeviceKind.stylus,
+          PointerDeviceKind.unknown,
+        },
+      ),
       title: 'Flutter Demo',
       theme: ThemeData(
         scaffoldBackgroundColor: Color(0xFFEDFFF4),
@@ -47,7 +51,6 @@ class MyHomePageState extends State<MyHomePage> {
   bool firstMessageSent = false;
   bool showQuickReplies = true; //show the quick replies
   bool chooseOptionDisplayed = false;
-  
 
   @override
   void initState() {
@@ -65,10 +68,15 @@ class MyHomePageState extends State<MyHomePage> {
   // Function to handle response and show quick replies after the first message
   void response(String query) async {
     try {
-      AuthGoogle authGoogle = await AuthGoogle(fileJson: "assets/healtcare_service.json").build();
-      DialogFlow dialogflow = DialogFlow(authGoogle: authGoogle, language: Language.english);
+      AuthGoogle authGoogle =
+          await AuthGoogle(
+            fileJson: "assets/jsons/healtcare_service.json",
+          ).build();
+      DialogFlow dialogflow = DialogFlow(
+        authGoogle: authGoogle,
+        language: Language.english,
+      );
       AIResponse aiResponse = await dialogflow.detectIntent(query);
-
 
       var messages = aiResponse.getListMessage();
       if (messages?.isNotEmpty ?? false) {
@@ -76,13 +84,17 @@ class MyHomePageState extends State<MyHomePage> {
         if (message != null && message is Map) {
           var textList = message["text"];
           if (textList != null && textList is List) {
-            var responseText = textList.isNotEmpty ? textList[0].toString() : 'No response';
+            var responseText =
+                textList.isNotEmpty ? textList[0].toString() : 'No response';
             setState(() {
               messsages.insert(0, {"data": 0, "message": responseText});
               firstMessageSent = true;
 
               if (!chooseOptionDisplayed) {
-                messsages.insert(0, {"data": 0, "message": "Choose an option:"});
+                messsages.insert(0, {
+                  "data": 0,
+                  "message": "Choose an option:",
+                });
                 chooseOptionDisplayed = true;
               }
             });
@@ -102,16 +114,15 @@ class MyHomePageState extends State<MyHomePage> {
       onPressed: () {
         setState(() {
           messsages.insert(0, {"data": 1, "message": option});
-          
-         
         });
         response(query);
       },
       style: OutlinedButton.styleFrom(
-        side: BorderSide(color: const Color.fromARGB(255, 11, 196, 140), width: 2),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
+        side: BorderSide(
+          color: const Color.fromARGB(255, 11, 196, 140),
+          width: 2,
         ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       ),
       child: Text(
         option,
@@ -120,140 +131,167 @@ class MyHomePageState extends State<MyHomePage> {
     );
   }
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      backgroundColor: Color.fromARGB(255, 75, 247, 132),
-      title: Center(
-        child: Text(widget.title),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 75, 247, 132),
+        title: Center(child: Text(widget.title)),
       ),
-    ),
-    body: Column(
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.only(top: 15, bottom: 10),
-          child: Text(
-            "Today, ${DateFormat("Hm").format(DateTime.now())}",
-            style: TextStyle(fontSize: 20),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 15, bottom: 10),
+            child: Text(
+              "Today, ${DateFormat("Hm").format(DateTime.now())}",
+              style: TextStyle(fontSize: 20),
+            ),
           ),
-        ),
-        Flexible(
-          child: ListView.builder(
-            reverse: true,
-            itemCount: messsages.length,
-            itemBuilder: (context, index) =>
-                chat(messsages[index]["message"].toString(), messsages[index]["data"]),
+          Flexible(
+            child: ListView.builder(
+              reverse: true,
+              itemCount: messsages.length,
+              itemBuilder:
+                  (context, index) => chat(
+                    messsages[index]["message"].toString(),
+                    messsages[index]["data"],
+                  ),
+            ),
           ),
-        ),
 
-if (firstMessageSent)
-  Padding(
-    padding: EdgeInsets.all(10.0),
-    child: Row(
-      children: [
-        // Hide/Show button 
-        IconButton(
-          icon: Icon(
-            showQuickReplies ? Icons.visibility : Icons.visibility_off,
-            color: Colors.greenAccent,
-          ),
-          onPressed: () {
-            setState(() {
-              showQuickReplies = !showQuickReplies; // Toggle visibility
-            });
-          },
-        ),
-        
-        // Quick reply buttons inside the SingleChildScrollView
-        if (showQuickReplies)
-          Expanded(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal, // Set to horizontal scrolling
+          if (firstMessageSent)
+            Padding(
+              padding: EdgeInsets.all(10.0),
               child: Row(
                 children: [
-                  _buildQuickReplyButton("Where can i find my vaccine information?", "Where can i find my vaccine information?"),
-                  SizedBox(width: 10),
-                  _buildQuickReplyButton("Where can i find the energy calculator?", "Where can i find the energy calculator?"),
-                  SizedBox(width: 10),
-                  _buildQuickReplyButton("What features does this app contain?", "What features does this app contain?"),
-                  SizedBox(width: 10),
-                  _buildQuickReplyButton("Where can i find my recipes?", "Where can i find my recipes?"),
-                  SizedBox(width: 10),
-                  _buildQuickReplyButton("Where can i find my oralhealth information?", "Where can i find my oralhealth information?"),
+                  // Hide/Show button
+                  IconButton(
+                    icon: Icon(
+                      showQuickReplies
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                      color: Colors.greenAccent,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        showQuickReplies =
+                            !showQuickReplies; // Toggle visibility
+                      });
+                    },
+                  ),
+
+                  // Quick reply buttons inside the SingleChildScrollView
+                  if (showQuickReplies)
+                    Expanded(
+                      child: SingleChildScrollView(
+                        scrollDirection:
+                            Axis.horizontal, // Set to horizontal scrolling
+                        child: Row(
+                          children: [
+                            _buildQuickReplyButton(
+                              "Where can i find my vaccine information?",
+                              "Where can i find my vaccine information?",
+                            ),
+                            SizedBox(width: 10),
+                            _buildQuickReplyButton(
+                              "Where can i find the energy calculator?",
+                              "Where can i find the energy calculator?",
+                            ),
+                            SizedBox(width: 10),
+                            _buildQuickReplyButton(
+                              "What features does this app contain?",
+                              "What features does this app contain?",
+                            ),
+                            SizedBox(width: 10),
+                            _buildQuickReplyButton(
+                              "Where can i find my recipes?",
+                              "Where can i find my recipes?",
+                            ),
+                            SizedBox(width: 10),
+                            _buildQuickReplyButton(
+                              "Where can i find my oralhealth information?",
+                              "Where can i find my oralhealth information?",
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
                 ],
               ),
             ),
-          ),
-      ],
-    ),
-  ),
 
-
-        Divider(height: 5.0, color: Colors.greenAccent),
-        ListTile(
-          leading: IconButton(
-            icon: Icon(Icons.camera_alt, color: Colors.greenAccent, size: 35),
-            onPressed: () {},
-          ),
-          title: Container(
-            height: 35,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              color: Color.fromRGBO(220, 220, 220, 1),
+          Divider(height: 5.0, color: Colors.greenAccent),
+          ListTile(
+            leading: IconButton(
+              icon: Icon(Icons.camera_alt, color: Colors.greenAccent, size: 35),
+              onPressed: () {},
             ),
-            padding: EdgeInsets.only(left: 15, bottom: 5),
-            child: TextField(
-              style: TextStyle(color: Colors.black),
-              controller: messageInsert,
-              decoration: InputDecoration(
-                hintText: "Enter a Message...",
-                hintStyle: TextStyle(color: const Color.fromARGB(255, 0, 0, 0)),
-                border: InputBorder.none,
+            title: Container(
+              height: 35,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Color.fromRGBO(220, 220, 220, 1),
+              ),
+              padding: EdgeInsets.only(left: 15, bottom: 5),
+              child: TextField(
+                style: TextStyle(color: Colors.black),
+                controller: messageInsert,
+                decoration: InputDecoration(
+                  hintText: "Enter a Message...",
+                  hintStyle: TextStyle(
+                    color: const Color.fromARGB(255, 0, 0, 0),
+                  ),
+                  border: InputBorder.none,
+                ),
               ),
             ),
+            trailing: IconButton(
+              icon: Icon(Icons.send, size: 30.0, color: Colors.greenAccent),
+              onPressed: () {
+                if (messageInsert.text.isNotEmpty) {
+                  setState(() {
+                    messsages.insert(0, {
+                      "data": 1,
+                      "message": messageInsert.text,
+                    });
+                  });
+                  response(messageInsert.text);
+                  messageInsert.clear();
+                  FocusScope.of(context).unfocus();
+                }
+              },
+            ),
           ),
-          trailing: IconButton(
-            icon: Icon(Icons.send, size: 30.0, color: Colors.greenAccent),
-            onPressed: () {
-              if (messageInsert.text.isNotEmpty) {
-                setState(() {
-                  messsages.insert(0, {"data": 1, "message": messageInsert.text});
-                });
-                response(messageInsert.text);
-                messageInsert.clear();
-                FocusScope.of(context).unfocus();
-              }
-            },
-          ),
-        ),
-        SizedBox(height: 15.0),
-      ],
-    ),
-  );
-}
-
+          SizedBox(height: 15.0),
+        ],
+      ),
+    );
+  }
 
   Widget chat(String message, int data) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 20),
       child: Row(
-        mainAxisAlignment: data == 1 ? MainAxisAlignment.end : MainAxisAlignment.start,
+        mainAxisAlignment:
+            data == 1 ? MainAxisAlignment.end : MainAxisAlignment.start,
         children: [
           data == 0
               ? SizedBox(
-                  height: 60,
-                  width: 60,
-                  child: CircleAvatar(
-                    backgroundImage: AssetImage("assets/robot.jpg"),
-                  ),
-                )
+                height: 60,
+                width: 60,
+                child: CircleAvatar(
+                  backgroundImage: AssetImage("assets/images/robot.jpg"),
+                ),
+              )
               : Container(),
           Padding(
             padding: EdgeInsets.all(10.0),
             child: Bubble(
               radius: Radius.circular(15.0),
-              color: data == 0 ? const Color.fromARGB(255, 11, 196, 140) : const Color.fromARGB(255, 11, 196, 140),
+              color:
+                  data == 0
+                      ? const Color.fromARGB(255, 11, 196, 140)
+                      : const Color.fromARGB(255, 11, 196, 140),
               child: Padding(
                 padding: EdgeInsets.all(2.0),
                 child: Row(
@@ -265,7 +303,10 @@ if (firstMessageSent)
                         constraints: BoxConstraints(maxWidth: 200),
                         child: Text(
                           message,
-                          style: TextStyle(color: const Color.fromARGB(255, 0, 0, 0), fontWeight: FontWeight.bold),
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 0, 0, 0),
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                     ),
@@ -276,12 +317,12 @@ if (firstMessageSent)
           ),
           data == 1
               ? SizedBox(
-                  height: 60,
-                  width: 60,
-                  child: CircleAvatar(
-                    backgroundImage: AssetImage("assets/default.jpg"),
-                  ),
-                )
+                height: 60,
+                width: 60,
+                child: CircleAvatar(
+                  backgroundImage: AssetImage("assets/images/default.jpg"),
+                ),
+              )
               : Container(),
         ],
       ),
