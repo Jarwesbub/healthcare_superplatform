fe584455d718c597b945fb84a220bb7ae37d6acb
Jarwesbub <jarwes90@gmail.com>
2025-04-01 15:35:01 +0300

    Added chatbot to eyesight demo

--- lib/demos/eyesight_stats/widgets/eyesight_bottom_navbar.dart
+++ lib/demos/eyesight_stats/widgets/eyesight_bottom_navbar.dart
@@ -26,7 +26,7 @@ class _NavigationBarState extends State<EyesightNavigationBar> {
         ),
         BottomNavigationBarItem(
           icon: Icon(FontAwesomeIcons.facebookMessenger),
-          label: 'Messages',
+          label: 'Chat',
         ),
         BottomNavigationBarItem(
           icon: Icon(Icons.account_circle),
